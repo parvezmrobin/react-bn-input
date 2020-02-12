1e82e612a6aaf2644dc612fd14da09a72b3cfd38
Parvez M Robin <parvezmrobin@gmail.com>
2020-02-13 02:11:30 +0600

    src/index renamed to src/App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './App.scss';
 import * as serviceWorker from './serviceWorker';
 import BnInput from "./BnInput";
 
@@ -9,4 +9,4 @@ ReactDOM.render(<BnInput autofocus/>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
